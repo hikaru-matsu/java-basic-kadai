b3c8e2c2cd732f01ffc2bd4911bf41d5b7d98168
Hikaru <mhikarum.080@gmail.com>
2025-08-24 16:23:05 +0900

    継承

---
+++ text.section_17/src/main/java/text/section_17/Keisyo_Human7.java
@@ -0,0 +1,5 @@
+package text.section_17;
+
+public abstract class Keisyo_Human7 {
+  abstract void output();
+}
---
+++ text.section_17/src/main/java/text/section_17/Keisyo_Polymorphism7.java
@@ -0,0 +1,11 @@
+package text.section_17;
+
+public class Keisyo_Polymorphism7 {
+  public static void main(String[] args) {
+    Keisyo_Kato7 kato = new Keisyo_Kato7();
+    Keisyo_Tanaka7 tanaka = new Keisyo_Tanaka7();
+
+    kato.output();
+    tanaka.output();
+  }
+}
